8b020af7a8f6dc329fc5e5450b718407461ebda0
vanshsaxena360 <100285405+vanshsaxena360@users.noreply.github.com>
2023-08-03 13:21:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
+import pymongo
 
 app = Flask(__name__)
 
@@ -72,15 +73,21 @@ def index():
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
+
+            client = pymongo.MongoClient("mongodb+srv://vanshsaxena360:vanshsaxena360@cluster0.sd6dmln.mongodb.net/?retryWrites=true&w=majority")
+            db = client['review_scrap']
+            review_col = db['review_scrap_data']
+            review_col.insert_many(reviews)
+
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             logging.info(e)
-            return 'something is wrong'
-    # return render_template('results.html')
+            return e
+    # return render_template('results.html')x   
 
     else:
         return render_template('index.html')
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
